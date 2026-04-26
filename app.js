e09364260af36a3e9621931e9fea35183f191783
KrTvGG <krtviv@mail.ru>
2026-04-26 18:56:37 +0300

    4.7. Шаблонные строки

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
-/*
-Ваша часовая ставка 80$ и вы готовы работать не
-более 5 часов в день 5 дней в неделю (кроме выходных).
-К вам приходит заказчик и предлагает заказ на 40
-часов работы.
-Сейчас понедельник.
-Вы должны уехать через 11 дней.
-Выведете в консоль:
-- Boolean переменную успеете ли вы взяться за работу
-- Сколько вы за неё попросите?
-*/
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий Пупкин';
 
-/** Моя часовая ставка в `$`*/
-const paymentInHour = 80;
-/** Максимальное кол-во часов, которве я готов тратить на работк в день */
-const maxWorkHoursInDay = 5;
-/** Кол-во дней до отпуска */
-const dayExist = 11;
-/** Кол-во часов для выполнения заказа */
-const orderHour = 40;
-/** Кол-во выходных в неделе */
-const daysDayOff = 2;
+const template = author + ' заказал ' + projectName + ' по цене ' + price + ' $';
+console.log(template);
 
-/** Максимально доступное кол-во рабочих дней до отпуска */
-const maxDays = dayExist - daysDayOff;
-/** Максимальное кол-во часов, которые у меня есть до отпуска */
-const maxHours = maxDays * maxWorkHoursInDay;
-/** Сумма, которую я запрошу за заказ */
-const resultPayment = paymentInHour * orderHour;
+const template2 = `${author} заказал ${projectName} по цене ${price} $`;
+console.log(template2);
 
-/** Успею ли я выполнить заказ */
-let toHaveTime = orderHour <= maxHours;
+const template3 = 'Проект \n' + 'Цена: ' + price + '$';
+console.log(template3);
 
-console.log('Успею ли я выполнить заказ до отпуска - ' + toHaveTime);
-
-console.log('Сколько я запрошу деняг за заказ - ' + resultPayment + '$');
+const template4 = `Проект
+Цена: ${price}`;
+console.log(template4);
