af676a371e8b24eab926624b3ba00cfa606c1b63
KrTvGG <krtviv@mail.ru>
2026-04-27 20:59:03 +0300

    5.3. Операторы равенства

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-/*
-    Вася положил 10 000$ на вклад 7% годовых с
-    копитализацией 1 раз в месяц.
-    Вывести в консоль, сможет ли он купить дом за 13 500$
-    через 2 года после снятия вклада. И остаток после покупки.
+const secretNumber = "7";
 
-    Итог = Сумма * (1 + Ставка в месяц не в %) ^ срок в месяц
-*/
-
-const moneyVasia = 12000;
-const rateProcentBank = 7;
-const costHouse = 13500;
-
-const result = moneyVasia * (1 + ((rateProcentBank / 100) / 12)) ** 24;
-
-if (costHouse <= result) {
-    console.log(`Вася сможет купить дом за ${costHouse}$ через 2 года`);
-    console.log(`Остаток Васи после покупки ${result - costHouse}$`);
-} else {
-    console.log(`Вася НЕ сможет купить дом за ${costHouse}$ через 2 года`);
+if (Number(secretNumber) === 7) {
+    console.log("Угадал строго");
 }
 
-console.log(result);
+if (secretNumber == 7) {
+    console.log("Угадал НЕ строго");
+}
