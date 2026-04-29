59634e3d2b1964aee34221447f28c7b4662b9a07
KrTvGG <krtviv@mail.ru>
2026-04-29 20:38:12 +0300

    7.3. Стрелочные функции

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ function powerOfTwo(num) {
 }
 console.log(powerOfTwo(5));
 
-const poft = function (num) {
-    return num * num;
-}
+const poft = (num) => num * num;
 console.log(poft(6));
+
+const poft2 = num => num * num;
+console.log(poft2(7));
+
+const poft3 = (num) => {
+    return num * num
+};
+console.log(poft3(8));
