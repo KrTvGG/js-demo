953d33b16d1d103e3c6cc3644c59418cf8a2b06d
KrTvGG <krtviv@mail.ru>
2026-04-29 18:57:22 +0300

    5.4. Switch

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-const secretNumber = "7";
+const role = "manager";
 
-if (Number(secretNumber) === 7) {
-    console.log("Угадал строго");
+switch (role) {
+    case "manager":
+        console.log("Менеджер");
+        break;
+    case "admin":
+        console.log("Admin");
+        break;
+    case "ceo":
+        console.log("ceo");
+        break;
+    default:
+        console.log("Мы тебя не знаем");
+        break;
 }
 
-if (secretNumber == 7) {
-    console.log("Угадал НЕ строго");
+switch (role) {
+    case "manager":
+    case "admin":
+        console.log("Не руководитель");
+        break;
+    case "ceo":
+        console.log("Руководитель");
+        break;
+    default:
+        console.log("Мы тебя не знаем");
+        break;
+}
+
+const num = 1;
+
+switch (true) {
+    case num > 0: // true === num > 0
+        console.log("Положительный");
+        break;
+    case num < 0:
+        console.log("Отрицатлельный");
+    default:
+        console.log("Ноль!");
 }
