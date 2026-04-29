106094085d57f409d57d053e15837a23be2b1210
KrTvGG <krtviv@mail.ru>
2026-04-29 19:18:24 +0300

    5.5. Тернарные операторы

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
-const role = "manager";
+const bmwX3Price = 100000;
+const fordFocusPrice = 10000;
+const budget = 20000;
 
-switch (role) {
-    case "manager":
-        console.log("Менеджер");
-        break;
-    case "admin":
-        console.log("Admin");
-        break;
-    case "ceo":
-        console.log("ceo");
-        break;
-    default:
-        console.log("Мы тебя не знаем");
-        break;
-}
+let message = budget > bmwX3Price ? 'BMW' : 'Велосипед';
 
-switch (role) {
-    case "manager":
-    case "admin":
-        console.log("Не руководитель");
-        break;
-    case "ceo":
-        console.log("Руководитель");
-        break;
-    default:
-        console.log("Мы тебя не знаем");
-        break;
-}
-
-const num = 1;
-
-switch (true) {
-    case num > 0: // true === num > 0
-        console.log("Положительный");
-        break;
-    case num < 0:
-        console.log("Отрицатлельный");
-    default:
-        console.log("Ноль!");
-}
+console.log(`Я могу купить ${message}`);
