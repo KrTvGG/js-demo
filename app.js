8ff3fb4812bc50ad5dfb0eae388782e8fe24a3b4
KrTvGG <krtviv@mail.ru>
2026-04-29 20:25:27 +0300

    7.1. Введение в функции

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-/*
-    Методом prompt получите ответ пользователя
-    на вопрос "Сколько будет 7 + или - 15?". Если ответ верен,
-    выведите в консоли "Успех", если нет - "Вы робот!",
-    а если он введёт "Я не робот", то тоже "Успех".
-*/
-
-const result = prompt("Сколько будет 7 + или - 15?");
-
-switch (true) {
-    case Number(result) === 22:
-    case Number(result) === -8:
-    case String(result) === "Я не робот":
-        console.log("Успех");
-        break;
-    default:
-        console.log("Вы робот!");
-        break;
+function logName(name, surname) {
+    console.log(`Мое имя ${name} ${surname}`);
 }
+
+logName('Иван', 'Крутов');
+
+function countDepositSum(depositInUSD, month, rate) {
+    return depositInUSD * (1 + rate / 12) ** month;
+}
+
+const example1 = countDepositSum(1000, 24, 0.12);
+console.log(example1);
+
+console.log(countDepositSum(1000, 48, 0.10));
