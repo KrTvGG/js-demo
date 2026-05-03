a6d18b81e4e9d161f09cb72474ecbc767eeeaf46
KrTvGG <krtviv@mail.ru>
2026-05-03 20:24:52 +0300

    8.6. Упражнение - Обновление списка задач (написал сам)

--- app.js
+++ app.js
@@ -1,7 +1,57 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+/*
+    Дан сримок задач
+    const tasks = ['Задача 1'];
+    Сделать функции:
+    - Добавление задач в конец
+    - Удаление задачи по названию
+    - Перенос задачи в начало списка по названию
+    Всегда менять исходный массив
+*/
 
-const url = 'auth/user/login';
-const res = url.split('/');
-console.log(res); // ["auth","user","login"]
+let tasks = ['Задача 1'];
 
-console.log(roles.join('-')); // user-admin-manager-superuser
+tasks = addTask(tasks, 'test');
+tasks = addTask(tasks, 'test2');
+tasks = addTask(tasks, 'test3');
+
+console.log(tasks);
+
+tasks = removeTaskByName(tasks, 'test3');
+
+console.log(tasks);
+
+tasks = taskPushTop(tasks, 'test2');
+
+console.log(tasks);
+
+function addTask(tasks, newTask) {
+    tasks.push(newTask);
+    return tasks;
+}
+
+function removeTaskByName(tasks, taskName) {
+    if (!tasks.includes(taskName)) {
+        return tasks;
+    }
+
+    const taskIndex = tasks.indexOf(taskName);
+    const startTasks = tasks.slice(0, taskIndex);
+    const endTasks = tasks.slice(taskIndex + 1);
+    const newTasks = startTasks.concat(endTasks);
+
+    return newTasks;
+}
+
+function taskPushTop(tasks, taskName) {
+    if (!tasks.includes(taskName)) {
+        return tasks;
+    }
+
+    const taskIndex = tasks.indexOf(taskName);
+    const startTasks = tasks.slice(0, taskIndex);
+    const endTasks = tasks.slice(taskIndex + 1);
+    const newTasks = startTasks.concat(endTasks);
+    newTasks.unshift(taskName);
+
+    return newTasks;
+}
