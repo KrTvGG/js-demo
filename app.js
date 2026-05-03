c4124afc0f4187c64d39d2e2deba2926cf6d4eea
KrTvGG <krtviv@mail.ru>
2026-05-03 19:03:48 +0300

    8.1. Знакомство с массивами

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
-/*
-    Пользователь:
-    - Возраст
-    - Налисие работы
-    - Деньги
-    Нужно проверить может ли он купить новый MacBook за 2000$?
-    Он может брать не только свои дениги, но и взять кредит
-    Ему дадут 500$, только если ему больше 24-х лет и он
-    имеет работу, 100$ если ему просто больше 24-х лет и 0 в
-    ином случае.
-    Напишите функцию, которая принимает данные пользователя
-    и товара и возвращает true или false;
-*/
+const roles = ["admin", "user", "superuser"];
 
-const mackbookCostUSD = 2000;
+const userInfo = ["Аня", 25];
 
-function credit(age, hasWork = false) {
-    switch (true) {
-	case age >= 24 && hasWork:
-		return 500;
-	case age >= 24:
-	    return 100;
-	default:
-	    return 0;
-    }
-}
+console.log(roles);
+console.log(roles[0]);
+console.log(roles[roles.length - 1]);
 
-function canBuy(age, hasWork, money) {
-    if (money < mackbookCostUSD) {
-        const creditValue = credit(age, hasWork);
-        money += creditValue;
-    }
+console.log(roles.at(0));
+console.log(roles.at(-1));
 
-    return money < mackbookCostUSD ? 'Не хватает денег на покупку!' : 'Хватет денег на покупку!';
-}
+const usersAge = [
+    2040 - 2022,
+    20 - '6',
+    10 > 0 ? 5 : 0
+];
+console.log(usersAge);
 
-const result = canBuy(24, true, 1500);
-
-console.log(result);
+const userNames = new Array('Вася', "Петя", "Катя");
+console.log(userNames);
