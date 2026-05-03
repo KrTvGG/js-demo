25f68085b3409271d6586ff310c0e9369a053106
KrTvGG <krtviv@mail.ru>
2026-05-03 17:08:40 +0300

    7.8. Упражнение - Кредит на MacBook

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-const KG_IN_USD = 7;
-const KM_IN_USD = 5;
+/*
+    Пользователь:
+    - Возраст
+    - Налисие работы
+    - Деньги
+    Нужно проверить может ли он купить новый MacBook за 2000$?
+    Он может брать не только свои дениги, но и взять кредит
+    Ему дадут 500$, только если ему больше 24-х лет и он
+    имеет работу, 100$ если ему просто больше 24-х лет и 0 в
+    ином случае.
+    Напишите функцию, которая принимает данные пользователя
+    и товара и возвращает true или false;
+*/
 
-function calculcateW(present) {
-    return present * KG_IN_USD;
+const mackbookCostUSD = 2000;
+
+function credit(age, hasWork = false) {
+    switch (true) {
+	case age >= 24 && hasWork:
+		return 500;
+	case age >= 24:
+	    return 100;
+	default:
+	    return 0;
+    }
 }
 
-function calculateKm(distance) {
-    return distance * KM_IN_USD;
+function canBuy(age, hasWork, money) {
+    if (money < mackbookCostUSD) {
+        const creditValue = credit(age, hasWork);
+        money += creditValue;
+    }
+
+    return money < mackbookCostUSD ? 'Не хватает денег на покупку!' : 'Хватет денег на покупку!';
 }
 
-function getExchangePrice(present1, present2, distance) {
-    const price1 = calculcateW(present1);
-    const price2 = calculcateW(present2);
-    const destancePrice = calculateKm(distance);
-    return price1 + price2 + destancePrice;
-}
+const result = canBuy(24, true, 1500);
 
-console.log(getExchangePrice(1, 2, 10));
+console.log(result);
