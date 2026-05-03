22661da7b5a3106496e4f41167f981e0b3e65460
KrTvGG <krtviv@mail.ru>
2026-05-03 19:39:09 +0300

    8.4. Slice, splice, concat, reverse

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-const roles = ['user', 'admin', 'manager'];
+const roles = ['user', 'admin', 'manager', 'superuser'];
 
-const elIndex = roles.indexOf('admin');
-console.log(elIndex); // 1
+const res = roles.slice(2);
+console.log(roles); // ["user","admin","manager","superuser"]
+console.log(res); // ["manager","superuser"]
 
-const elIndex2 = roles.indexOf('superuser');
-console.log(elIndex2); // -1
+const res2 = roles.slice(2, 3);
+console.log(res2); // ["manager"]
 
-if (roles.indexOf('admin') >= 0) {
-    console.log('Доступ есть!');
-}
+const res3 = roles.slice(-1);
+console.log(res3); // ["superuser"]
+const res4 = roles.slice(-2);
+console.log(res4); // ["manager","superuser"]
 
-console.log(roles.includes('admin')); // true
-console.log(roles.includes('superuser')); // false
+const res5 = roles.slice(1, -1);
+console.log(res5); // ["admin","superuser"]
 
-if (roles.includes('admin')) {
-    console.log('Доступ есть!');
-}
+// const res6 = roles.splice(2);
+// console.log(res6); // ["manager","superuser"]
+// console.log(roles); // ["user","admin"]
+
+// const res7 = roles.splice(2, 1);
+// console.log(res7); // ["manager"]
+
+// const res8 = roles.splice(-1);
+// console.log(res8); // ["superuser"]
+
+const res9 = roles.reverse();
+console.log(res9); // ["superuser","manager","admin","user"]
+console.log(roles); // ["superuser","manager","admin","user"]
+
+const newRoles = ['sysadmin', 'developer'];
+const res10 = roles.concat(newRoles);
+console.log(res10); // ["superuser","manager","admin","user","sysadmin","developer"]
