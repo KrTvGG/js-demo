a6c11b2dc03f21bd64840c8f6639dadc69fd10ee
KrTvGG <krtviv@mail.ru>
2026-05-04 20:53:24 +0300

    8.8. Деструктуризация

--- app.js
+++ app.js
@@ -1,39 +1,8 @@
-/*
-    Дан сримок задач
-    const tasks = ['Задача 1'];
-    Сделать функции:
-    - Добавление задач в конец
-    - Удаление задачи по названию
-    - Перенос задачи в начало списка по названию
-    Всегда менять исходный массив
-*/
-
-const tasks = ['Задача 1'];
-
-function Add(task) {
-    tasks.push(task);
+function getData() {
+    return ['Антон', 16, "Москва"]
 }
 
-function Remove(task) {
-    const index = tasks.indexOf(task);
-    if (index === -1) {
-        return;
-    }
-    return tasks.splice(index, 1);
-}
+const [userName, age, city] = getData();
 
-function Prioritize(task) {
-    const result = Remove(task);
-    if (!result) {
-        return;
-    }
-    tasks.unshift(result[0]);
-}
+console.log(userName, age, city);
 
-Add('Задача 2');
-Add('Задача 3');
-console.log(tasks); // ["Задача 1","Задача 2","Задача 3"]
-Remove('Задача 2');
-console.log(tasks); // ["Задача 1","Задача 3"]
-Prioritize('Задача 3');
-console.log(tasks); // ["Задача 3","Задача 1"]
