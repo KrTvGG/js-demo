32686dca632b946328e272ff1d87e37121dec97b
KrTvGG <krtviv@mail.ru>
2026-05-04 22:30:33 +0300

    9.7. Упражнение - Расчёт итогового баланса (реализовал сам)

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-const arr = [1, 4, 8, 7];
+/*
+    Есть выгрузка операций пользователя
+    const operations = [1000, -700, 300, -500, 10000];
+    а так же начальный баланс в 100$
+    Необходимо сделать фунеции расчёта:
+    - Итогового баланса
+    - Наличия отрицательного баланса (если после очередной операции
+        баланс < 0, то выдавать false)
+    - Расчёт среднего расхода и среднего дохода
+*/
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+const operations = [1000, -700, 300, -500, 10000];
+
+let balance = 100;
+
+function haveNegativeBalance() {
+    if (balance < 0) return false;
+
+    return true;
 }
 
-for (let element of arr) {
-    console.log(element);
+function calculateBalance() {
+    let i = 0;
+    do {
+        balance = balance + operations[i];
+        i++;
+    } while (haveNegativeBalance() && i < operations.length);
 }
 
-for (let index in arr) {
-    console.log(arr[index]);
+function calculateAVGExpenceAndIncome() {
+    let positive = 0;
+    let negative = 0;
+
+    for (i of operations) {
+        if (i > 0) {
+            positive = positive + i;
+        } else {
+            negative = negative + i;
+        }
+    }
+
+    return [positive, negative];
 }
+
+calculateBalance();
+
+const [positive, negative] = calculateAVGExpenceAndIncome();
+
+console.log(balance);
+console.log(positive);
+console.log(negative);
