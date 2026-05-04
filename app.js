75e7381dfc34d5b687bfc8659dc209b22c0dda05
KrTvGG <krtviv@mail.ru>
2026-05-04 21:33:02 +0300

    9.1. Цикл for

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-/*
-    Дан произвольный url -
-    https://purpleshool.ru/course/javascript
-    Нужно сделать функцию, которая выводит в консоль:
-    - Протокол (https)
-    - Доменное имя (purpleshool.ru)
-    - Путь внутри сайта (/course/javascript)
-*/
-
-const url = 'https://purpleshool.ru/course/javascript';
-
-parseUrl(url);
-
-function parseUrl(url) {
-    const [protocol, otherUrl] = url.split(':');
-    const [none1, none2, domain, ...patchArray] = otherUrl.split('/');
-    const patch = patchArray.join('/')
-    console.log(protocol);
-    console.log(domain);
-    console.log(patch);
+for (let i = 1; i < 10; i++) {
+    console.log(`Наш баланс ${i}$`);
 }
