09a27c9e4c0b219e7e2a6d8696b91e1f9a51ab6e
KrTvGG <krtviv@mail.ru>
2026-05-08 20:56:36 +0300

    11.8. Упражнение - реализация some

--- app.js
+++ app.js
@@ -9,3 +9,5 @@ const some = (arr, el) => {
 }
 console.log(some(arr, 1)); // false
 console.log(some(arr, 2)); // true
+console.log(arr.some(el => el === 1)); // false
+console.log(arr.some(el => el === 2)); // true
