9ec580945c6cb7f12991a446b10f1d5e1f12efab
KrTvGG <krtviv@mail.ru>
2026-05-08 20:08:34 +0300

    11.3. filter

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const score = [5, 10, 0, 15];
+const operations = [100, -20, 7, -20, 50];
+// const positiveOperations = [];
+// for (const operation of operations) {
+//     if (operation > 0) {
+//         positiveOperations.push(operation);
+//     }
+// }
+// console.log(positiveOperations);
 
-score.forEach((scoreEl, i, arr) => {
-    console.log(`Райнд ${i + 1}: ${scoreEl}`);
+const positiveOperations = operations.filter((operation) => {
+    return operation > 0;
 });
+console.log(positiveOperations);
 
-const transactionInUSD = [10, -7, 50, -10, 100];
-
-const transactionInRUB = [];
-for (const transaction of transactionInUSD) {
-    transactionInRUB.push(transaction * 60);
-}
-
-console.log(transactionInUSD);
-console.log(transactionInRUB);
-
-const transactionInRUB2 = transactionInUSD
-    .map((transaction, i, arr) => {
-        return transaction * 60;
-    });
-
-console.log(transactionInRUB2);
+const positiveRUBOperations = operations
+    .filter((operation) => {
+        return operation > 0;
+    })
+    .map(operation => operation * 60);
+console.log(positiveRUBOperations);
