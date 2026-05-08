5478394ccc5a61b6abf5e4d45c63c29e488d09c4
KrTvGG <krtviv@mail.ru>
2026-05-08 21:16:12 +0300

    11.10. sort

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-const prices = [[2, 4], [3, 4], [10, [20, 50]]];
-const res = prices.flat(2); // Глубина вложенности
-console.log(res); // [2, 4, 3, 4, 10, 20, 50]
-const res2 = prices.flatMap(el => el.concat([1]));
-console.log(res2); // [2, 4, 1, 3, 4, 1, 10, Array(2), 1]
+const users = ['Вася', 'Маша', 'Катя', 'Аня'];
+console.log(users); // ['Вася', 'Маша', 'Катя', 'Аня']
+users.sort();
+console.log(users); // ['Аня', 'Вася', 'Катя', 'Маша']
+
+const operations = [100, -300, -100, 50, 480];
+console.log(operations); // [100, -300, -100, 50, 480]
+operations.sort();
+console.log(operations); // [-100, -300, 100, 480, 50]
+
+// < 0 - el1, el2 - сохраняем порядок
+// > 0 - el2, el1 - меняем порядок
+// По возрастанию
+operations.sort((el1, el2) => {
+    if (el1 > el2) {
+        return 1;
+    }
+    if (el1 < el2) {
+        return -1;
+    }
+});
+console.log(operations); // [-300, -100, 50, 100, 480]
+
+// По убыванию
+operations.sort((el1, el2) => {
+    if (el1 < el2) {
+        return 1;
+    }
+    if (el1 > el2) {
+        return -1;
+    }
+});
+console.log(operations); // [480, 100, 50, -100, -300]
+
+// По возрастанию (сокращенный вариант)
+operations.sort((el1, el2) => el1 - el2);
+console.log(operations); // [-300, -100, 50, 100, 480]
