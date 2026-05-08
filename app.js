e8b34691036b00dc1511bebdccda9c7533756742
KrTvGG <krtviv@mail.ru>
2026-05-08 21:30:00 +0300

    11.11. Быстрое создание массивов

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
-const users = ['Вася', 'Маша', 'Катя', 'Аня'];
-console.log(users); // ['Вася', 'Маша', 'Катя', 'Аня']
-users.sort();
-console.log(users); // ['Аня', 'Вася', 'Катя', 'Маша']
+const arr = [1, 2, 3, 4, 5];
 
-const operations = [100, -300, -100, 50, 480];
-console.log(operations); // [100, -300, -100, 50, 480]
-operations.sort();
-console.log(operations); // [-100, -300, 100, 480, 50]
+console.log(new Array(1, 2, 3, 4, 5)); // [1, 2, 3, 4, 5]
+const arr2 = new Array(5);
+console.log(arr2); // [пусто × 5]
+arr2.fill(1, 0, 3);
+console.log(arr2); // [1, 1, 1, пусто × 2]
+arr2.fill(2, 3, 5);
+console.log(arr2); // [1, 1, 1, 2, 2]
 
-// < 0 - el1, el2 - сохраняем порядок
-// > 0 - el2, el1 - меняем порядок
-// По возрастанию
-operations.sort((el1, el2) => {
-    if (el1 > el2) {
-        return 1;
-    }
-    if (el1 < el2) {
-        return -1;
-    }
-});
-console.log(operations); // [-300, -100, 50, 100, 480]
-
-// По убыванию
-operations.sort((el1, el2) => {
-    if (el1 < el2) {
-        return 1;
-    }
-    if (el1 > el2) {
-        return -1;
-    }
-});
-console.log(operations); // [480, 100, 50, -100, -300]
-
-// По возрастанию (сокращенный вариант)
-operations.sort((el1, el2) => el1 - el2);
-console.log(operations); // [-300, -100, 50, 100, 480]
+const arr3 = Array.from({ length: 5 }, (cur, i) => i + 1);
+console.log(arr3); // [1, 2, 3, 4, 5]
