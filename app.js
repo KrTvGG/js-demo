7bdff031392ce75eb68f56df06e2bff4ce4207df
KrTvGG <krtviv@mail.ru>
2026-05-08 20:00:27 +0300

    11.2. map

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const score = [5, 10, 0, 15];
 
-for (const [i, el] of score.entries()) {
-    console.log(`Райнд ${i + 1}: ${el}`);
+score.forEach((scoreEl, i, arr) => {
+    console.log(`Райнд ${i + 1}: ${scoreEl}`);
+});
+
+const transactionInUSD = [10, -7, 50, -10, 100];
+
+const transactionInRUB = [];
+for (const transaction of transactionInUSD) {
+    transactionInRUB.push(transaction * 60);
 }
 
-console.log('--------');
+console.log(transactionInUSD);
+console.log(transactionInRUB);
 
-score.forEach((el, i) => {
-    console.log(`Райнд ${i + 1}: ${el}`);
-});
+const transactionInRUB2 = transactionInUSD
+    .map((transaction, i, arr) => {
+        return transaction * 60;
+    });
+
+console.log(transactionInRUB2);
