fdeeef581cee8648994bac04f47ff4ade8d7473f
KrTvGG <krtviv@mail.ru>
2026-05-11 17:04:45 +0300

    12.4. Преобразование строки

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-/* вытащить имя и фамилию в отдельгые переменные */
-const userName = 'Вася aka Terminator Perdinator Пупкин';
+const str = 'Вася Пупкинa';
+console.log(str.includes('а')); // true
+console.log(str.startsWith('В')); // true
+console.log(str.endsWith('н')); // true
+console.log(new String('Вася Пупкин').includes('а')); // true
 
-const firstSpaceIndex = userName.indexOf(' ');
-const firstName = userName.slice(0, firstSpaceIndex);
-const lastSpaceIndex = userName.lastIndexOf(' ');
-const lastName = userName.slice(lastSpaceIndex + 1, userName.length);
-console.log(`${firstName} ${lastName}`);
+console.log(str.toLowerCase()); // вася пупкин
+console.log(str.toUpperCase()); // ВАСЯ ПУПКИН
+console.log(str.replace('В', 'Ф')); // Фася Пупкин
+console.log(str.replace('П', 'Д')); // Вася Дупкин
+console.log(str.replace(/а/g, 'и')); // Вися Пупкинa
+
+const str2 = ' Вася Пупкинa  \n';
+console.log(str2.trim()); // Вася Пупкинa
+console.log(str2.trimEnd()); // _Вася Пупкинa
+console.log(str2.trimStart()); //  Вася Пупкинa__
